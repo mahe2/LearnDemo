6d4fcc115abd4066b05c871d4abfd28326de4713
mahe <512014999@qq.com>
2017-09-19 18:19:40 +0800

    集成leckcancy

--- learndemo/src/main/java/com/learn/demo/framework/LeApplication.java
+++ learndemo/src/main/java/com/learn/demo/framework/LeApplication.java
@@ -2,9 +2,20 @@ package com.learn.demo.framework;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by mahe on 2017/8/30.
  */
 
 public class LeApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
 }
