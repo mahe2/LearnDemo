1a491a6a6b8b4b28fc518381905835b386346eee
mahe <512014999@qq.com>
2017-09-27 15:40:47 +0800

    修改启动页逻辑

--- learndemo/src/main/java/com/learn/demo/activity/SplashActivity.java
+++ learndemo/src/main/java/com/learn/demo/activity/SplashActivity.java
@@ -2,7 +2,8 @@ package com.learn.demo.activity;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.util.Log;
+import android.os.Message;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.launcher.ARouter;
@@ -19,35 +20,49 @@ import butterknife.ButterKnife;
 
 //@EActivity(R.layout.activity_splash_activity_layout)
 public class SplashActivity extends LeActivity {
+    private static final int MSG_COUNT_DOWN = 1;
     @BindView(R.id.channel_textview) TextView mChannelTextView;
     @BindView(R.id.count_down_textview) TextView mCountDownTextView;
-    private String channel;
+    private String channel = "debug";
+    private int countDown = 5;
+
+    private Handler splashHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MSG_COUNT_DOWN:
+                    if(countDown == 1){
+                        entryMain();
+                    }else{
+                        countDown -- ;
+                        mCountDownTextView.setText(getString(R.string.count_down_text,String.valueOf(countDown)));
+                        splashHandler.sendEmptyMessageDelayed(MSG_COUNT_DOWN,1000);
+                    }
+                    break;
+            }
+        }
+    };
+
     @Override
     protected void onCreateActivityImpl() {
         setContentView(R.layout.activity_splash_activity_layout);
         ARouter.init(getApplication());
-
-        channel = PackerNg.getChannel(getApplication());
-        Log.i("channel",channel+"");
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                entryMain();
-            }
-        },3000);
+        channel = TextUtils.isEmpty(PackerNg.getChannel(getApplication())) ? "debug" : PackerNg.getChannel(getApplication());
         ButterKnife.bind(this);
         initView();
+        splashHandler.sendEmptyMessageDelayed(MSG_COUNT_DOWN,1000);
     }
 
     private void initView(){
         mChannelTextView.setText(getString(R.string.channel_text,channel));
-        mCountDownTextView.setText(getString(R.string.count_down_text));
+        mCountDownTextView.setText(getString(R.string.count_down_text,String.valueOf(countDown)));
     }
 
 
     private void entryMain(){
         Intent mainIntent = new Intent(this,EntryActivity.class);
         startActivity(mainIntent);
+        finish();
 //        ARouter.getInstance().build("/test_arouter/").navigation();
     }
 }
