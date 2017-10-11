2609597bc558ef8f3d7eea56d241bbfb540e1649
mahe <512014999@qq.com>
2017-10-11 17:59:17 +0800

    添加EventBus框架

--- learndemo/src/main/java/com/learn/demo/activity/EventBusActivity.java
+++ learndemo/src/main/java/com/learn/demo/activity/EventBusActivity.java
@@ -5,9 +5,17 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.learn.demo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
+import de.greenrobot.event.Subscribe;
+import de.greenrobot.event.ThreadMode;
 
 /**
  * Created by mahe on 2017/8/30.
@@ -15,12 +23,31 @@ import com.learn.demo.R;
 
 @Route(path = "/eventbus/")
 public class EventBusActivity extends AppCompatActivity {
+    @BindView(R.id.event_bus_textview) TextView mEventBusTextView;
+    @BindView(R.id.event_bus_post) Button mEventBusPostBtn;
 
-    Button testDebugBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventbus_activity_layout);
+        EventBus.getDefault().register(this);
+        ButterKnife.bind(this);
+    }
 
+    @OnClick(R.id.event_bus_post)
+    public void postEventBusMsg(View view){
+        EventBus.getDefault().post("test event bus");
+    }
+
+    @Subscribe(threadMode = ThreadMode.MainThread)
+    public void helloEventBus(String msg){
+        mEventBusTextView.setText(msg);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
