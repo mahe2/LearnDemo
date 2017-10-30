8d7abaf967d10095532c0030bebd46d642b2a8aa
mahe <512014999@qq.com>
2017-10-30 16:22:42 +0800

    测试lambda 表达式

--- learndemo/src/main/java/com/learn/demo/activity/LambdaActivity.java
+++ learndemo/src/main/java/com/learn/demo/activity/LambdaActivity.java
@@ -1,12 +1,18 @@
 package com.learn.demo.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.learn.demo.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by mahe on 2017/8/30.
@@ -20,5 +26,26 @@ public class LambdaActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lambda_activity_layout);
+        initData();
+    }
+
+
+
+    private void initView(){
+
+    }
+
+    private void initData(){
+        Runnable runnable = () -> {
+            Log.i("lamb","hello word");
+        };
+
+        new Thread(runnable).start();
+
+    }
+
+
+    private void log(String msg){
+        Log.i("lamb",msg);
     }
 }
